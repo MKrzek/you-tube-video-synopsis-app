1a7d578a29621139c4cf9d547ca46943b05f4d9d
MKrzek <mkrzek@googlemail.com>
2025-03-16 20:08:04 +0000

    feat: add custom Head component

---
+++ src/components/AppHead.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+
+const AppHead = () => {
+    return <Head>
+        <title>You Tube video synopsis app</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Paste a url to your chosen youTube video and get a summary of it" />
+        <link rel="icon" href="/public/icons/icon_192x192.png" />
+      </Head>
+ }
+
+export default AppHead;
