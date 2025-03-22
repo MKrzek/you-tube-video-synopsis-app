703a5c051a467df828ddb9a5c0a3a1f5d390c436
MKrzek <mkrzek@googlemail.com>
2025-03-22 18:50:04 +0000

    feat: initialise email sending

--- src/pages/api/sendByEmail/index.ts
+++ src/pages/api/sendByEmail/index.ts
@@ -1,8 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import sgMail from '@sendgrid/mail';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
 
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+    const msg = {
+      to: 'test@example.com',
+      from: 'test@example.com',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    await sgMail.send(msg)
   } catch (e) {
 
   }
